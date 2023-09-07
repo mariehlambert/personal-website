4f262a62598789e9e1a914f427fdeb9be73193f0
Saba Karimeddiny <sabakarimeddiny@gmail.com>
2023-09-06 21:55:35 -0400

    form

---
+++ form_process.php
@@ -0,0 +1,71 @@
+<?php
+if (isset($_POST['email'])) {
+
+    // REPLACE THIS 2 LINES AS YOU DESIRE
+    $email_to = "marielambert59@gmail.com";
+    $email_subject = "You have a message from" . clean_string($email);
+
+    function problem($error)
+    {
+        echo "Oh looks like there is some problem with your form data: <br><br>";
+        echo $error . "<br><br>";
+        echo "Please fix those to proceed.<br><br>";
+        die();
+    }
+
+    // validation expected data exists
+    if (
+        !isset($_POST['fullName']) ||
+        !isset($_POST['email']) ||
+        !isset($_POST['message'])
+    ) {
+        problem('Oh looks like there is some problem with your form data.');
+    }
+
+    $name = $_POST['fullName']; // required
+    $email = $_POST['email']; // required
+    $message = $_POST['message']; // required
+
+    $error_message = "";
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+
+    if (!preg_match($email_exp, $email)) {
+        $error_message .= 'Email address does not seem valid.<br>';
+    }
+
+    $string_exp = "/^[A-Za-z .'-]+$/";
+
+    if (!preg_match($string_exp, $name)) {
+        $error_message .= 'Name does not seem valid.<br>';
+    }
+
+    if (strlen($message) < 2) {
+        $error_message .= 'Message should not be less than 2 characters<br>';
+    }
+
+    if (strlen($error_message) > 0) {
+        problem($error_message);
+    }
+
+    function clean_string($string)
+    {
+        $bad = array("content-type", "bcc:", "to:", "cc:", "href");
+        return str_replace($bad, "", $string);
+    }
+
+    $email_message = clean_string($name) . "says:\n" . clean_string($message);
+
+    // create email headers
+    $headers = 'From: ' . $email . "\r\n" .
+        'Reply-To: ' . $email . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+    @mail($email_to, $email_subject, $email_message, $headers);
+?>
+
+    <!-- Replace this as your success message -->
+
+    Thanks for contacting us, we will get back to you as soon as possible.
+
+<?php
+}
+?>
